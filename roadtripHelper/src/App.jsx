d0e6ae8287976c83a6dfc453b60137bf2ed3db84
Richard <richardlwhittington@gmail.com>
2024-07-12 15:46:38 -0700

    initial commit

---
+++ roadtripHelper/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [click, setClick] = useState(0)
+
+  let stadiums = {
+    
+  }
+
+  return (
+    <>
+    <form>
+      <input></input>
+      <input></input>
+      <input></input>
+      <input></input>
+      <input></input>
+      <button>Press if you Dare!</button>
+      </form>
+    </>
+  )
+}
+
+export default App
