904032b6d3b96cec247093b50a4c160bd0e6bf05
Richard <richardlwhittington@gmail.com>
2024-07-19 15:24:32 -0500

    add object array

---
+++ backend/server.js/server.js
@@ -0,0 +1,21 @@
+// const stadiums = [
+// {
+//     name: 'Busch',
+//     team: 'Cardinals',
+//     schedule: ['june 15', 'june 16', 'july 16', 'july 17', 'july 18']
+// },
+// {
+//     name: 'Truist',
+//     team: 'Braves',
+//     schedule: ['june 15', 'june 16', 'july 16', 'july 7', 'july 8']
+// },
+// {
+//     name: 'Wringley',
+//     team: 'Cubs',
+//     schedule: ['june 15', 'june 16', 'july 16', 'july 1', 'july 2']
+// },
+// {
+//     name: 'GR',
+//     team: 'White Soxs',
+//     schedule: ['june 15', 'june 16', 'july 17', 'july 18', 'july 19']
+// },]
--- roadtripHelper/src/App.jsx
+++ roadtripHelper/src/App.jsx
@@ -4,9 +4,29 @@ import './App.css'
 function App() {
   const [click, setClick] = useState(0)
 
-  let stadiums = {
-    
-  }
+  const stadiums = 
+    [
+      {
+          name: 'Busch',
+          team: 'Cardinals',
+          schedule: ['june 15', 'june 16', 'july 16', 'july 17', 'july 18']
+      },
+      {
+          name: 'Truist',
+          team: 'Braves',
+          schedule: ['june 15', 'june 16', 'july 16', 'july 7', 'july 8']
+      },
+      {
+          name: 'Wringley',
+          team: 'Cubs',
+          schedule: ['june 15', 'june 16', 'july 16', 'july 1', 'july 2']
+      },
+      {
+          name: 'GR',
+          team: 'White Soxs',
+          schedule: ['june 15', 'june 16', 'july 17', 'july 18', 'july 19']
+      },]
+  
 
   return (
     <>
