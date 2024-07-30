454912bd2a2e12b9358d145bf363f6e914cb8301
Richard <richardlwhittington@gmail.com>
2024-07-30 16:30:03 -0500

    format code for readibility

--- roadtripHelper/src/App.jsx
+++ roadtripHelper/src/App.jsx
@@ -1,54 +1,97 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [teamOne, setTeamOne] = useState('')
-  const [teamTwo, setTeamTwo] = useState('')
-  const [teamThree, setTeamThree] = useState('')
-  const [teamFour, setTeamFour] = useState('')
-  const [dateOne, setDateOne] = useState('')
-  const [dateTwo, setDateTwo] = useState('')
+  const [teamOne, setTeamOne] = useState("");
+  const [teamTwo, setTeamTwo] = useState("");
+  const [teamThree, setTeamThree] = useState("");
+  const [teamFour, setTeamFour] = useState("");
+  const [dateOne, setDateOne] = useState("");
+  const [dateTwo, setDateTwo] = useState("");
 
-  const stadiums = 
-    [
-      {
-          name: 'Busch',
-          team: 'Cardinals',
-          schedule: ['june 15', 'june 16', 'july 16', 'july 17', 'july 18']
-      },
-      {
-          name: 'Truist',
-          team: 'Braves',
-          schedule: ['june 15', 'june 16', 'july 16', 'july 7', 'july 8']
-      },
-      {
-          name: 'Wringley',
-          team: 'Cubs',
-          schedule: ['june 15', 'june 16', 'july 16', 'july 1', 'july 2']
-      },
-      {
-          name: 'GR',
-          team: 'White Soxs',
-          schedule: ['june 15', 'june 16', 'july 17', 'july 18', 'july 19']
-      },]
-  
-      const handleSubmit = () => {}
+  const stadiums = [
+    {
+      name: "Busch",
+      team: "Cardinals",
+      schedule: ["june 15", "june 16", "july 16", "july 17", "july 18"],
+    },
+    {
+      name: "Truist",
+      team: "Braves",
+      schedule: ["june 15", "june 16", "july 16", "july 7", "july 8"],
+    },
+    {
+      name: "Wringley",
+      team: "Cubs",
+      schedule: ["june 15", "june 16", "july 16", "july 1", "july 2"],
+    },
+    {
+      name: "GR",
+      team: "White Soxs",
+      schedule: ["june 15", "june 16", "july 17", "july 18", "july 19"],
+    },
+  ];
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
 
   return (
     <>
-    <form onSubmit={ handleSubmit }>
-      <label>Team One:<input type='text' value={teamOne} onChange={e => setTeamOne(e.target.value)}></input></label>
-      <label>Team Two:<input type='text' value={teamTwo} onChange={e => setTeamTwo(e.target.value)}></input></label>
-      <label>Team Three:<input type='text' value={teamThree} onChange={e => setTeamThree(e.target.value)}></input></label>
-      <label>Team Four:<input type='text' value={teamFour} onChange={e => setTeamFour(e.target.value)}></input></label>
-      <label>Dates:<input type='date' value={dateOne} onChange={e => setDateOne(e.target.value)}></input>-<input  type='date' value={dateTwo} onChange={e => setDateTwo(e.target.value)}></input></label>
-      <button type='submit'>Press If You Dare!</button>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Team One:
+          <input
+            type="text"
+            value={teamOne}
+            onChange={(e) => setTeamOne(e.target.value)}
+          ></input>
+        </label>
+        <label>
+          Team Two:
+          <input
+            type="text"
+            value={teamTwo}
+            onChange={(e) => setTeamTwo(e.target.value)}
+          ></input>
+        </label>
+        <label>
+          Team Three:
+          <input
+            type="text"
+            value={teamThree}
+            onChange={(e) => setTeamThree(e.target.value)}
+          ></input>
+        </label>
+        <label>
+          Team Four:
+          <input
+            type="text"
+            value={teamFour}
+            onChange={(e) => setTeamFour(e.target.value)}
+          ></input>
+        </label>
+        <label>
+          Dates:
+          <input
+            type="date"
+            value={dateOne}
+            onChange={(e) => setDateOne(e.target.value)}
+          ></input>
+          -
+          <input
+            type="date"
+            value={dateTwo}
+            onChange={(e) => setDateTwo(e.target.value)}
+          ></input>
+        </label>
+        <button type="submit">Press If You Dare!</button>
       </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 // const ProfileForm = () => {
 //   const { isAuthenticated } = useAuth0();
 //   const [firstName, setFirstName] = useState('');
@@ -71,9 +114,9 @@ export default App
 //         'Content-Type': 'application/json'
 //       },
 //       body: JSON.stringify(newUser)
-      
+
 //     })
-   
+
 //       .then(response => response.json())
 //       .then(data => console.log(data))
 //       .catch(error => console.error(error));
