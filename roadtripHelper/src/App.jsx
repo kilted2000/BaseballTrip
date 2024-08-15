1ea202d80d0c17539150c33b01824d33dfb4ebed
Richard <richardlwhittington@gmail.com>
2024-08-15 17:01:25 -0500

    begin adding react-hook-form to react form

--- roadtripHelper/src/App.jsx
+++ roadtripHelper/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-
+import { useForm } from "react-hook-form"
 function App() {
   const [teamOne, setTeamOne] = useState("");
   const [teamTwo, setTeamTwo] = useState("");
@@ -9,6 +9,12 @@ function App() {
   // const [dateOne, setDateOne] = useState("");
   // const [dateTwo, setDateTwo] = useState("");
   const [results, setResults] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
   
   const stadiums = [
     {
@@ -33,7 +39,7 @@ function App() {
     },
   ];
 
-  const handleSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
     const teams = [teamOne, teamTwo, teamThree, teamFour].filter(Boolean);
 
@@ -49,7 +55,7 @@ function App() {
 
   return (
     <div className="bg-[url('./assets/stadium.jpg')] bg-cover bg-no-repeat h-dvh justify-center justify-items-center items-center flex flex-col">
-      <form onSubmit={handleSubmit} className="rounded-lg bg-gradient-to-r from-emerald-700 via-teal-700 to-cyan-600 bg-no-repeat p-9 shadow-2xl shadow-green-900 space-y-4 flex flex-col gap-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="rounded-lg bg-gradient-to-r from-emerald-700 via-teal-700 to-cyan-600 bg-no-repeat p-9 shadow-2xl shadow-green-900 space-y-4 flex flex-col gap-4">
         <div>
         <label className="w-1/2">
           Team:
@@ -116,3 +122,87 @@ function App() {
 
 export default App;
 
+// import { useState, useEffect } from 'react';
+// import './App.css';
+// import styles from './Card.module.css';
+
+// const Card = () => {
+//   const [data, setData] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&q=chicken≈';
+//       const options = {
+//         method: 'GET',
+//         headers: {
+//           'X-RapidAPI-Key': import.meta.env.VITE_REACT_API_KEY,
+//           'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+//         },
+//       };
+
+//       try {
+//         const response = await fetch(url, options);
+//         if (!response.ok) {
+//           throw new Error('API request failed');
+//         }
+//         const data = await response.json();
+//         console.log(data);
+//         setData(data.results);
+//         setIsLoading(false);
+//       } catch (error) {
+//         setError(error);
+//         setIsLoading(false);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+  
+
+//   if (isLoading) return <div>Loading...</div>;
+//   if (error) return <div>Error: {error.message}</div>;
+//   if (data.length === 0) return <div>No data available</div>;
+//   return (
+   
+//         <div className={styles.cardGrid}>
+//     <div className={styles.cardsContainer}>
+//    {data.map((recipe) => {
+//       if (!recipe.name || !recipe.description) {
+//         console.warn(`Recipe is missing name or description: ${recipe}`);
+//         return null;
+//       }
+//       return (
+      
+//         <div
+//           className={styles.card}
+//           style={{
+//             backgroundImage: `url(${recipe.thumbnail_url})`,
+//             backgroundSize: 'cover',
+//             backgroundPosition: 'center',
+//           }}
+//           key={recipe.id}
+//         >
+//           <h2 className={styles.cardName}>{recipe.name}</h2>
+//         </div>
+       
+ 
+//         // <div className={styles.cardGrid}>
+//         // <div className={styles.cards}  style={{
+//         //   backgroundImage: `url(${recipe.thumbnail_url})`,
+//         //   height: '334px',
+//         //   width: '242px',
+//         //   backgroundSize: 'cover',
+//         //   backgroundPosition: 'center',
+//         // }} key={recipe.id}>
+//         //   <h2 className={styles.cardName}>{recipe.name}</h2>
+//         // </div>
+//         // </div>
+//       );
+//     })}
+//     </div>
+//   </div>
+// )
+
+// }
+// export default Card;
