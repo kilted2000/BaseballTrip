ebe4c90025c9131ebf0927426e60bd63f7f646b1
Richard <richardlwhittington@gmail.com>
2024-09-04 00:46:46 -0500

    format date

--- backend/src/main/java/com/example/backend/games/Game.java
+++ backend/src/main/java/com/example/backend/games/Game.java
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
  public record Game(String Day, String HomeTeam) { 
     public String getDayInCustomFormat() {
-        DateTimeFormatter inputFormatter = DateTimeFormatter.ISO_DATE;
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
         DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("EEE, d MMM", Locale.ENGLISH);
         
         LocalDate date = LocalDate.parse(Day, inputFormatter);
