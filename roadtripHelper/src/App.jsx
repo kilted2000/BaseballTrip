507e4146de01da471ed3ccd01c2dcce935520409
Richard <richardlwhittington@gmail.com>
2024-09-30 11:19:30 -0500

    add clerk auth to frontend

--- roadtripHelper/src/App.jsx
+++ roadtripHelper/src/App.jsx
@@ -1,15 +1,30 @@
-
-import React from 'react';
-import './App.css'
-import GameFinder from './components/GameFinder';
+import React from "react";
+import "./App.css";
+import GameFinder from "./components/GameFinder";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 function App() {
   return (
+    <>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
       <div className="App">
           <header className="App-header">
               <GameFinder />
           </header>
-      </div>
-  )
+      </div> 
+    </>
+  );
 }
 
-export default App
+export default App;
