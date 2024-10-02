c2ba1b1a514fb313c7fcadd2984c2076b110bb2b
Richard <richardlwhittington@gmail.com>
2024-10-02 01:09:33 -0500

    style landing page

--- roadtripHelper/src/components/LandingPage.jsx
+++ roadtripHelper/src/components/LandingPage.jsx
@@ -2,8 +2,10 @@ import { SignedOut, SignInButton } from "@clerk/clerk-react";
 
 const LandingPage = () => {
   return (
-    <div className="bg-emerald-900">
-      <h1 className="bg-gray-300">Welcome to the Baseball Jungle!</h1>
+    <div className="bg-emerald-900 bg-gradient-to-r from-cyan-500 to-blue-500
+    bg-contain h-dvh bg-center">
+      <h1 className="text-2xl text-gray-300 text-center ">Welcome to the Baseball Jungle!</h1>
+      <img src="toronto.jpeg" className="m-auto rounded-lg"></img>
     </div>
   );
 };
