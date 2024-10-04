789febf4e7bf4c07af9e9a69225e75155cdac38e
Richard <richardlwhittington@gmail.com>
2024-10-04 00:03:20 -0500

    create navbar

--- roadtripHelper/src/App.jsx
+++ roadtripHelper/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <SignedOut>
-        <SignInButton />
+        {/* <SignInButton /> */}
         <LandingPage />
       </SignedOut>
 
--- roadtripHelper/src/components/LandingPage.jsx
+++ roadtripHelper/src/components/LandingPage.jsx
@@ -2,10 +2,14 @@ import { SignedOut, SignInButton } from "@clerk/clerk-react";
 
 const LandingPage = () => {
   return (
-    <div className="bg-emerald-900 bg-gradient-to-r from-cyan-500 to-blue-500
+    <div className="bg-emerald-900 bg-gradient-to-r from-indigo-800 to-emerald-900
     bg-contain h-dvh bg-center">
-      <h1 className="text-2xl text-gray-300 text-center ">Welcome to the Baseball Jungle!</h1>
-      <img src="toronto.jpeg" className="m-auto rounded-lg"></img>
+      <div class="navbar bg-base-300">
+  <a class="btn btn-ghost text-xl ">Road Trip Helper</a>
+  <SignInButton className='absolute top-0 right-0 mt-4 mr-4 text-sky-500'/>
+</div>
+      <h1 className="text-2xl text-gray-300 text-center mt-16">Welcome to the Baseball Jungle!</h1>
+      <img src="toronto.jpeg" className="m-auto rounded-lg mt-20"></img>
     </div>
   );
 };
