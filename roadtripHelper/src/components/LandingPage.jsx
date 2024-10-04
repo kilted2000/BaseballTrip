31813d0ba7dc510194c561d19b0b41b6f768fab3
Richard <richardlwhittington@gmail.com>
2024-10-04 02:14:20 -0500

    style landing page and add navbar

--- roadtripHelper/src/components/LandingPage.jsx
+++ roadtripHelper/src/components/LandingPage.jsx
@@ -8,10 +8,4 @@ const LandingPage = () => {
   <a class="btn btn-ghost text-xl ">Road Trip Helper</a>
   <SignInButton className='absolute top-0 right-0 mt-4 mr-4 text-sky-500'/>
 </div>
-      <h1 className="text-2xl text-gray-300 text-center mt-16">Welcome to the Baseball Jungle!</h1>
-      <img src="toronto.jpeg" className="m-auto rounded-lg mt-20"></img>
-    </div>
-  );
-};
-
-export default LandingPage;
+      <h1 className="text-
