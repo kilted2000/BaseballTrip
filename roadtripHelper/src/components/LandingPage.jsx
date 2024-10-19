6ff72ef50030324ecac4f0b496304ee3ecba70fb
Richard <richardlwhittington@gmail.com>
2024-10-18 23:18:01 -0500

    restore landing css

--- roadtripHelper/src/components/LandingPage.jsx
+++ roadtripHelper/src/components/LandingPage.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import './landingPage.module.css';  // Ensure you have your CSS file linked
-
+import { SignInButton } from "@clerk/clerk-react";
+import styles from "./landingPage.module.css"
 const LandingPage = () => {
   return (
-    <div>
-      <header className="header">
-        <h1>Baseball-Trip Helper</h1>
-        <div className="main-title">
-          <h2>Simplify your road-trip planning!</h2>
-        </div>
-      </header>
-
-      <section className="media">
-        <h4>As Seen On:</h4>
-        <p>CNN</p>
-      </section>
-
-      <section className="middle">
-        <h3>How Baseball-Trip Helper works</h3>
-        <p>
-          Lorem Ipsum I love a parade. tihjchshfjhfkfsfsfbjkjhkjhklblbclbblkjbcbjbhbjbkjbbjbjbbjbljbj.
-        </p>
-      </section>
-
-      <footer className="footer">
-        <div className="data">
-          <div className="cta">
+    <div
+      className="bg-emerald-900 bg-gradient-to-r from-indigo-800 to-emerald-900
+    bg-contain h-dvh bg-center h-full"
+    >
+      <div className="navbar bg-base-300">
+        <a className="btn btn-ghost text-xl ">Baseball Bucketlist</a>
+        <SignInButton className="absolute top-0 right-0 mt-4 mr-4 text-sky-500" />
+      </div>
+      <div className={styles.header}>
+       <h1>Baseball-Trip Helper</h1>
+       <div className={styles.mainTitle}>
+        <h2>Simplify your road-trip planning!</h2>
+       </div>
+       
+    </div>
+    <div className={styles.media}>
+    <h4>As Seen On:</h4>
+    <p>CNN</p>
+    </div>
+    <div className={styles.middle}>
+     <h3>How Baseball-Trip Helper works"</h3>
+     <p>Lorem Ipsom I love a parade. tihjchshfjhfkfsfsfbjkjhkjhklblbclbblkjbcbjbhbjbkjbbjbjbbjbljbj</p>
+    </div>
+    <div className={styles.footer}>
+        <div className={styles.data}>
+        <div className={styles.cta}>
             <p>Sign Up or Sign In Now!</p>
-          </div>
-          <nav>
-            <a href="https://facebook.com">Facebook</a>
-            <a href="https://twitter.com">Twitter</a>
-            <a href="https://instagram.com">Instagram</a>
-            <a href="https://github.com">GitHub</a>
-          </nav>
         </div>
-      </footer>
+        <a>Facebook</a>
+        <a>Twitter</a>
+        <a>Instagram</a>
+        <a>GitHub</a>
+    </div>
+    </div>
     </div>
   );
 };
