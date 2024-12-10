9a230f9428ce4a05a1a55ea219390ba5ecda71cb
Richard <richardlwhittington@gmail.com>
2024-12-10 06:04:07 -0600

    edit

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Ensure ncurses is installed for tput support
-apt-get update && apt-get install -y ncurses-base
+# Install tput dependencies
+apt-get update && apt-get install -y ncurses-utils
 
 # Install Java without interactive prompts
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 export sdkman_color_enable=false
 sdk install java 23.0.1-amzn
+
 
 
 
