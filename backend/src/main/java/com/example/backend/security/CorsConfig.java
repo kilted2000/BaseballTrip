4b77867fc82f28faa32b94964d7755f80cc6c6ba
Richard <richardlwhittington@gmail.com>
2025-02-09 18:32:06 -0600

    reedit cors sec

--- backend/src/main/java/com/example/backend/security/CorsConfig.java
+++ backend/src/main/java/com/example/backend/security/CorsConfig.java
@@ -1,21 +1,26 @@
 package com.example.backend.security;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
-
+    
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings( @SuppressWarnings("null") CorsRegistry registry) {
-                registry.addMapping("/**") 
-                        .allowedOrigins("http://localhost:5173", "https://roadtriphelper.netlify.app", "https://roadtriphelper.com") 
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
-                        .allowedHeaders("*") 
-                        .a
+public WebMvcConfigurer corsConfigurer(){
+    return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry){
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173", "https://roadtriphelper.netlify.app", "http://roadtriphelper.com")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
+    };
+}
+
+    
+}
