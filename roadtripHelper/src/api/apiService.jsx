b0998ec4a80dc4dc6cf9768ccafd7c90727e07d5
Richard <richardlwhittington@gmail.com>
2025-02-13 13:50:16 -0600

    log url to test

--- roadtripHelper/src/api/apiService.jsx
+++ roadtripHelper/src/api/apiService.jsx
@@ -1,6 +1,6 @@
 
 const API_URL = process.env.VITE_BACKEND_URL;
-
+console.log(API_URL);
 export const getGames = async () => {
     try {
         console.log("Fetching games...");
@@ -24,6 +24,4 @@ export const getGames = async () => {
         return data;
     } catch (error) {
         console.error('Error fetching games:', error);
-        throw error;
-    }
-};
+        t
