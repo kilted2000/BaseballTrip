47339276762b6a2634ef5917c02c6c9f25e9a6a6
Richard <richardlwhittington@gmail.com>
2025-02-13 13:18:22 -0600

    reset global bean

--- backend/src/main/java/com/example/backend/controller/GameController.java
+++ backend/src/main/java/com/example/backend/controller/GameController.java
@@ -2,14 +2,13 @@ package com.example.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.model.GameModel;
 import com.example.backend.service.GameService;
 
-@CrossOrigin(origins = {"http://localhost:5173", "https://roadtriphelper.netlify.app"})
+//@CrossOrigin(origins = {"http://localhost:5173", "https://roadtriphelper.netlify.app"})
 @RestController
 public class GameController {
 
