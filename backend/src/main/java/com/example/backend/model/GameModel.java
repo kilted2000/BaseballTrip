94a67c50675a01db3db2d1638b5bbdb1a368c4e8
Richard <richardlwhittington@gmail.com>
2025-02-24 06:29:31 -0600

    update gameid

--- backend/src/main/java/com/example/backend/model/GameModel.java
+++ backend/src/main/java/com/example/backend/model/GameModel.java
@@ -1,17 +1,23 @@
 package com.example.backend.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
+@JsonIgnoreProperties(ignoreUnknown = true) 
 public class GameModel {
 
-    @Id
-    @JsonProperty("GameID") 
-    private Long gameId;
+     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id; 
+    @JsonProperty("GameID")
+    private Long gameId; 
      @JsonProperty("HomeTeam")
     private String HomeTeam;
     @JsonProperty("Date")
@@ -24,6 +30,14 @@ public class GameModel {
         this.gameId = gameId;
         this.HomeTeam = HomeTeam;
         this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Long getGameId() {
