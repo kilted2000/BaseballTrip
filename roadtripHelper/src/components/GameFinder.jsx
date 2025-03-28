481174b6813743f6b59211257d2d77c4f3a1b351
Richard <richardlwhittington@gmail.com>
2025-03-28 02:08:37 -0500

    make fr double

--- roadtripHelper/src/components/GameFinder.jsx
+++ roadtripHelper/src/components/GameFinder.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
@@ -20,14 +19,16 @@ const GameFinder = ({ setIsLoading, setResults, setShowForm }) => {
       teamOne: "",
       teamTwo: "",
       teamThree: "",
-      teamFour: ""
+      teamFour: "",
     },
   });
 
   const formatTeamName = (input) => {
     const formattedInput = input.trim().toLowerCase();
-    const team = teams.find(({ name, city, nickname }) => 
-      [name.toLowerCase(), city.toLowerCase(), nickname.toLowerCase()].includes(formattedInput)
+    const team = teams.find(({ name, city, nickname }) =>
+      [name.toLowerCase(), city.toLowerCase(), nickname.toLowerCase()].includes(
+        formattedInput
+      )
     );
     return team ? team.abbreviation : null;
   };
@@ -74,9 +75,9 @@ const GameFinder = ({ setIsLoading, setResults, setShowForm }) => {
       setIsLoading(false);
     }
   };
-//className="bg-gradient-to-r from-emerald-700 via-teal-700 to-cyan-600 bg-no-repeat shadow-2xl shadow-green-900 place-items-center card-body"
+  //className="bg-gradient-to-r from-emerald-700 via-teal-700 to-cyan-600 bg-no-repeat shadow-2xl shadow-green-900 place-items-center card-body"
 
-//className="grid gap-3 md:grid-cols-2 md:gap-4"
+  //className="grid gap-3 md:grid-cols-2 md:gap-4"
   return (
     <div>
       <div className="bg-[url('/stadium.jpg')] bg-cover bg-repeat-y object-cover justify-center items-center flex flex-col h-dvh">
@@ -87,123 +88,166 @@ const GameFinder = ({ setIsLoading, setResults, setShowForm }) => {
           </div>
         </div>
 
-        <form onSubmit={handleSubmit(onSubmit)} style={{
-        background: "linear-gradient(to right, #047857, #0d9488, #0e7490)",
-        backgroundRepeat: "no-repeat",
-        boxShadow: "0 25px 50px -12px rgba(22, 163, 74, 0.5)", 
-        display: "grid",
-        placeItems: "center",
-        padding: "1.5rem" 
-      }}>
-          <div sx={{
-          display: "grid",
-          gap: 2,
-          gridTemplateColumns: { xs: "1fr" },
-          "@media (min-width: 600px)": {
-            gridTemplateColumns: '2fr, 2fr',
-            gap: 4
-          }
-        }}>
-     
-          
-        <Controller
-          name="teamOne"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              
-              GameFinder
-              options={teams.map((option) => option.name)}
-              onChange={(_, value) => field.onChange(value)}
-              value={field.value || ""}
-              sx={{ width: "100%" }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  {...field}
-                  onChange={(e) => field.onChange(e.target.value)}
-                  placeholder="Team One"
-                  variant="outlined"
-                  sx={{
-                    "& .MuiOutlinedInput-root": {
-                      borderRadius: "0.5rem", 
-                      paddingLeft: "0.5rem", 
-                      backgroundColor: "white" 
-                    }
-                  }}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          style={{
+            background: "linear-gradient(to right, #047857, #0d9488, #0e7490)",
+            backgroundRepeat: "no-repeat",
+            boxShadow: "0 25px 50px -12px rgba(22, 163, 74, 0.5)",
+            display: "grid",
+            placeItems: "center",
+            padding: "1.5rem",
+          }}
+        >
+          <div
+            sx={{
+              display: "grid",
+              gap: 2,
+              gridTemplateColumns: { xs: "1fr" },
+              "@media (min-width: 600px)": {
+                gridTemplateColumns: "2fr, 2fr",
+                gap: 4,
+              },
+            }}
+          >
+            <Controller
+              name="teamOne"
+              control={control}
+              render={({ field }) => (
+                <Autocomplete
+                  GameFinder
+                  options={teams.map((option) => option.name)}
+                  onChange={(_, value) => field.onChange(value)}
+                  value={field.value || ""}
+                  sx={{ width: "100%" }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      {...field}
+                      onChange={(e) => field.onChange(e.target.value)}
+                      placeholder="Team One"
+                      variant="outlined"
+                      sx={{
+                        "& .MuiOutlinedInput-root": {
+                          borderRadius: "0.5rem",
+                          paddingLeft: "0.5rem",
+                          backgroundColor: "white",
+                        },
+                      }}
+                    />
+                  )}
                 />
               )}
             />
-          )}
-        />
-        <Controller
-          name="teamTwo"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              
-              GameFinder
-              options={teams.map((option) => option.name)}
-              onChange={(_, value) => field.onChange(value)}
-              value={field.value || ""}
-              sx={{ width: "100%" }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  {...field}
-                  onChange={(e) => field.onChange(e.target.value)}
-                  placeholder="Team Two"
-                  variant="outlined"
-                  sx={{
-                    "& .MuiOutlinedInput-root": {
-                      borderRadius: "0.5rem", 
-                      paddingLeft: "0.5rem", 
-                      backgroundColor: "white" 
-                    }
-                  }}
+            <Controller
+              name="teamTwo"
+              control={control}
+              render={({ field }) => (
+                <Autocomplete
+                  GameFinder
+                  options={teams.map((option) => option.name)}
+                  onChange={(_, value) => field.onChange(value)}
+                  value={field.value || ""}
+                  sx={{ width: "100%" }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      {...field}
+                      onChange={(e) => field.onChange(e.target.value)}
+                      placeholder="Team Two"
+                      variant="outlined"
+                      sx={{
+                        "& .MuiOutlinedInput-root": {
+                          borderRadius: "0.5rem",
+                          paddingLeft: "0.5rem",
+                          backgroundColor: "white",
+                        },
+                      }}
+                    />
+                  )}
                 />
               )}
             />
-          )}
-        />
-        <Controller
-          name="teamThree"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              
-              GameFinder
-              options={teams.map((option) => option.name)}
-              onChange={(_, value) => field.onChange(value)}
-              value={field.value || ""}
-              sx={{ width: "100%" }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  {...field}
-                  onChange={(e) => field.onChange(e.target.value)}
-                  placeholder="Team Three"
-                  variant="outlined"
-                  sx={{
-                    "& .MuiOutlinedInput-root": {
-                      borderRadius: "0.5rem", 
-                      paddingLeft: "0.5rem", 
-                      backgroundColor: "white" 
-                    }
-                  }}
+            <Controller
+              name="teamThree"
+              control={control}
+              render={({ field }) => (
+                <Autocomplete
+                  GameFinder
+                  options={teams.map((option) => option.name)}
+                  onChange={(_, value) => field.onChange(value)}
+                  value={field.value || ""}
+                  sx={{ width: "100%" }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      {...field}
+                      onChange={(e) => field.onChange(e.target.value)}
+                      placeholder="Team Three"
+                      variant="outlined"
+                      sx={{
+                        "& .MuiOutlinedInput-root": {
+                          borderRadius: "0.5rem",
+                          paddingLeft: "0.5rem",
+                          backgroundColor: "white",
+                        },
+                      }}
+                    />
+                  )}
                 />
               )}
             />
-          )}
-        />
-        <Controller
-          name="teamFour"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              
-              GameFinder
-              options={teams.map((option) => option.name)}
-              onChange={(_, value) => field.onChange(value)}
-              value={field.value || ""}
-              sx={{ w
+            <Controller
+              name="teamFour"
+              control={control}
+              render={({ field }) => (
+                <Autocomplete
+                  GameFinder
+                  options={teams.map((option) => option.name)}
+                  onChange={(_, value) => field.onChange(value)}
+                  value={field.value || ""}
+                  sx={{ width: "100%" }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      {...field}
+                      onChange={(e) => field.onChange(e.target.value)}
+                      placeholder="Team Four"
+                      variant="outlined"
+                      sx={{
+                        "& .MuiOutlinedInput-root": {
+                          borderRadius: "0.5rem",
+                          paddingLeft: "0.5rem",
+                          backgroundColor: "white",
+                        },
+                      }}
+                    />
+                  )}
+                />
+              )}
+            />
+          </div>
+          <label className="w-full mr-5">Dates:</label>
+          <DatePicker
+            onChange={(ranges) =>
+              setDateRange({
+                startDate: ranges.selection.startDate,
+                endDate: ranges.selection.endDate,
+              })
+            }
+          />
+          <div className="card-actions">
+            <button
+              type="submit"
+              className="bg-blue-700 hover:bg-blue-900 self-center cursor-pointer rounded-full text-stone-100 px-3 pb-2"
+            >
+              Press If You Dare!
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default GameFinder;
