78f09b9008542d5736a56731b3179895a90950ee
Richard <richardlwhittington@gmail.com>
2025-05-19 14:10:25 -0500

    clean up HC

--- backend/src/main/java/com/example/backend/controller/HealthController.java
+++ backend/src/main/java/com/example/backend/controller/HealthController.java
@@ -2,11 +2,9 @@ package com.example.backend.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HealthController {
 
     @GetMapping("/ping")
